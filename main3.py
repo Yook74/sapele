0e28a4bbcc831f111784ed5402dd85686cb2428c
carl young <carlyoungflutes@gmail.com>
2022-11-04 15:22:53 -0600

    created flute class

--- main3.py
+++ main3.py
@@ -1,14 +1,44 @@
 from my_flute_class import MyFlute
 
 
+def select_script() -> int:
+    """Selects script to run"""
+    while True:
+        value = input(f'SELECT NUMBER: ')
+        if not value:
+            exit()
+
+        try:
+            return int(value)
+        except ValueError:
+            print('Invalid Input')
+
+
+def main():
+    flute = MyFlute.flute_key()
+    print(flute.print_sizing())
+
+    while True:
+
+        print(f'****************************')
+        print(f'1. Get Blank Sizing        *')
+        print(f'2. Get Tuner Ref Offset    *')
+        print(f'3. Data Entry (Pre-Tune)   *')
+        print(f'4. FH Entry (Post-Tune)    *')
+        print(f'****************************')
+
+        num_select = select_script()
+
+        if num_select == 1:
+            flute.print_sizing()
+        if num_select == 2:
+            print()
+        if num_select == 3:
+            print()
+        if num_select == 4:
+            flute.get_finger_hole_placements()
+            flute.print_fh_placement()
+
+
 if __name__ == '__main__':
-
-    print(f'----------------------------')
-    flute = MyFlute.get_blank_sizing()
-
-    flute.print_sizing()
-
-    flute.get_finger_hole_placements()
-
-    flute.print_fh_placement()
-
+    main()
--- my_flute_class.py
+++ my_flute_class.py
@@ -1,3 +1,5 @@
+import csv
+import linecache
 
 
 class MyFlute:
@@ -14,6 +16,9 @@ class MyFlute:
         self.drone_width = drone_width
         self.drone_bore_center = drone_bore_center
         self.fh_values = []
+        self.key = str()
+        self.octave = int()
+        self.fudge_factor = float()
 
     def print_sizing(self):
         print(f'----------------------------')
@@ -44,7 +49,49 @@ class MyFlute:
         for index, holes in enumerate(self.fh_values):
             print(f'FH_{index+1}: {holes:.2f}')
 
+    @classmethod
+    def flute_key(cls):
+        key = input('Enter the flute Key: ')
+        octave = input('Enter the key Octave: ')
+        if not octave:
+            octave = 4
+
+        my_key = key + '_' + octave
+        with open('records/initialSetup.csv') as data:
+
+            my_file = csv.DictReader(data)
+
+            for row in my_file:
+                if row['Key'] == my_key:
+                    bore_diameter = float(dict.pop(row, 'bore'))
+                    wall_thickness = float(dict.pop(row, 'wall'))
+                    dl_factor = float(dict.pop(row, 'd/l'))
+                    fudge = float(dict.pop(row, 'fudge'))
+                    bore_length = bore_diameter * dl_factor
+                    blank_length = bore_length + 7
+                    blank_height = bore_diameter + wall_thickness + fudge
+                    single_width = bore_diameter + (wall_thickness * 2) + fudge
+                    drone_width = (bore_diameter * 2) + (wall_thickness * 4)
+                    drone_bore_center = (bore_diameter / 2) + wall_thickness
+                    return cls(bore_length, bore_diameter, wall_thickness, dl_factor, blank_length, blank_height,
+                               single_width, drone_width, drone_bore_center)
+
+        if not row:
+            bore_diameter = float(input('Enter bore diameter: '))
+            wall_thickness = float(input('Enter wall thickness: '))
+            dl_factor = float(input('Enter wall D/L ratio: '))
+            fudge_factor = float(input('Enter fudge factor: '))
+            bore_length = bore_diameter * dl_factor
+            blank_length = bore_length + 7
+            blank_height = bore_diameter + wall_thickness + fudge_factor
+            single_width = bore_diameter + (wall_thickness * 2) + fudge_factor
+            drone_width = (bore_diameter * 2) + (wall_thickness * 4)
+            drone_bore_center = (bore_diameter / 2) + wall_thickness
+            return cls(bore_length, bore_diameter, wall_thickness, dl_factor, blank_length, blank_height,
+                       single_width, drone_width, drone_bore_center)
+
     def get_finger_hole_placements(self):
+        self.fh_values.clear()
         self.bore_length = float(input('Enter the actual bore length: '))
         finger_hole_percents = [.68, .62, .56, .48, .40, .33]
         for percent in finger_hole_percents:
