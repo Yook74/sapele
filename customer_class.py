6e95cb47e3227b24f61d036cd5f014e4ca9bbeaf
carl young <carlyoungflutes@gmail.com>
2022-12-22 22:11:44 -0700

    created new customer class

---
+++ customer_class.py
@@ -0,0 +1,18 @@
+import csv
+from math import sqrt
+import linecache
+
+
+class Customer:
+
+    def input_order(self):
+        """ name, date, type, Key, Octave, tuning, wood, price """
+        pass
+
+    def show_orders(self):
+        """ show all, all completed, current, include sales totals """
+        pass
+
+    def update_order(self):
+        """ """
+        pass
